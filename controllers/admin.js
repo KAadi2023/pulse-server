bab43b28b088d0cead442f7ec5ea4aacc01ffeee
Aditya Kumar <aditya.naxtre@gmail.com>
2024-07-16 23:03:33 +0530

    made api to get all messages and dashboard stats

--- controllers/admin.js
+++ controllers/admin.js
@@ -72,10 +72,74 @@ const allChats = TryCatch(async (req, res) => {
     })
 })
 
-const allMessages = TryCatch(async (req, res) => { });
+const allMessages = TryCatch(async (req, res) => {
+    const messages = await Message.find({})
+        .populate("sender", "name avatar")
+        .populate("chat", "groupChat")
+
+    const transformedMessages = messages.map(({ _id, content, attachments, sender, createdAt, chat }) => ({
+        _id,
+        content,
+        attachments,
+        createdAt: new Date(createdAt).toISOString(),
+        sender: {
+            _id: sender._id,
+            name: sender.name,
+            avatar: sender.avatar.url
+        },
+        chat: {
+            _id: chat._id,
+            groupChat: chat.groupChat,
+        }
+    }))
+
+    return res.status(200).json({
+        success: true,
+        messages: transformedMessages,
+    })
+});
+
+const getDashboardStats = TryCatch(async (req, res) => {
+    const [totalUsers, totalChats, totalMessages, totalGroups] = await Promise.all([
+        User.countDocuments(),
+        Chat.countDocuments(),
+        Message.countDocuments(),
+        Chat.countDocuments({ groupChat: true }),
+    ])
+    const today = new Date();
+    const last7days = new Date();
+    last7days.setDate(last7days.getDate() - 7);
+
+    const last7daysMessages = await Message.find({
+        createdAt: { $gte: last7days, $lt: today }
+    }).select("createdAt");
+
+    const messages = new Array(7).fill(0);
+
+    const daysInMilliseconds = 1000 * 60 * 60 * 24
+
+    last7daysMessages.forEach(message => {
+        const indexApprox = (today.getTime() - message.createdAt.getTime()) / daysInMilliseconds
+        const dayIndex = Math.floor(indexApprox);
+        messages[6 - dayIndex]++;
+    });
+
+    const stats = {
+        totalUsers,
+        totalChats,
+        totalMessages,
+        totalGroups,
+        messagesChart: messages
+    }
+    return res.status(200).json({
+        success: true,
+        stats
+    })
+})
 
 export {
     allUsers,
     allChats,
     allMessages,
+    getDashboardStats,
 }
